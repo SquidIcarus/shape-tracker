af03005bcdf5c2faa885ad12dfe66264c5f74843
Landon Isaiah <landonisaiah@gmail.com>
2023-12-19 00:18:24 -0800

    test getArea function.

--- __tests__/rectangle.test.js
+++ __tests__/rectangle.test.js
@@ -6,5 +6,10 @@ describe('Rectangle', () => {
         const rectangle = new Rectangle(3, 5);
         expect(rectangle.side1).toEqual(3);
         expect(rectangle.side2).toEqual(5);
+    })
+
+    test('should correctly get the area of a rectangle object', () => {
+        const rectangle = new Rectangle(3, 5);
+        expect(rectangle.getArea()).toEqual(15);
     });
-})
+}); 
